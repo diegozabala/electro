1072a6619bc18581f0f8e20b26e68d7b96ee0424
Diego Alberto Zabala Betancur <diegozabalab@gmail.com>
2016-11-17 20:08:51 -0500

    Cambia el css
    http://www.colorhexa.com/485e88

--- resources/views/admin/template/partials/footer.blade.php
+++ resources/views/admin/template/partials/footer.blade.php
@@ -1,30 +1,100 @@
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-<footer class="footer-distributed">
-      <div class="footer-left">
-      <img src="{{asset('images/uniquindio.png')}}" class="img-responsive" alt="Uniquindio">
+<!--footer-->
+<footer class="footer1">
+<div class="container">
+
+    <div class="row"><!-- row -->  
+
+        <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+            <ul class="list-unstyled clear-margins"><!-- widgets -->      
+                <li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    <h1 class="title-widget">Uniquindio</h1>
+                    <ul>
+                        <img src="{{asset('images/uniquindio.png')}}" class="img-responsive" alt="Uniquindio">
+                    </ul>
+                </li>         
+            </ul>
+        </div><!-- widgets column left end -->
+
+        <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+            <ul class="list-unstyled clear-margins"><!-- widgets -->      
+                <li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    <h1 class="title-widget">Se realizo con:</h1>
+                    <ul>
+                      <li><a href="https://laravel.com/docs/5.2" target="_blank"><i class="fa fa-angle-double-right"></i> Laravel 5.2</a></li>
+                        <li><a href="http://php.net/" target="_blank"><i class="fa fa-angle-double-right"></i> PHP >= 5.5.9</a></li>
+                        <li><a href="https://getcomposer.org/" target="_blank"><i class="fa fa-angle-double-right"></i> Composer</a></li>
+                        <li><a href="http://bootsnipp.com/" target="_blank"><i class="fa fa-angle-double-right"></i> Bootsnipp</a></li>
+                        <li><a href="http://www.html5.com/" target="_blank"><i class="fa fa-angle-double-right"></i> HTML 5</a></li>
+                        <li><a href="https://www.javascript.com/" target="_blank"><i class="fa fa-angle-double-right"></i>  JavaScript</a></li>
+                        <li><a href="https://www.mysql.com/" target="_blank"><i class="fa fa-angle-double-right"></i>  MySQL</a></li>
+                    </ul>
+                </li>         
+            </ul>
+        </div><!-- widgets column left end -->
+
+        <div class="col-lg-3 col-md-3"><!-- widgets column center -->
+            <ul class="list-unstyled clear-margins"><!-- widgets -->
+                <li class="widget-container widget_recent_news"><!-- widgets list -->
+                    <h1 class="title-widget">Detalles de Contacto </h1>
+                    <div class="footerp"> 
+                        <h2 class="title-median">Facultad de Ingenieria</h2>
+                        <p><b>Numeros de Ayuda</b>
+                        <b style="color:#ffc106;">(8AM to 10PM):</b>  3217294966, +91-8130190010 </p>
+                        <p><b>Email: </b> <a href="https://www.google.com/intl/es-419/mail/help/about.html" target="_blank">dazabalab@uqvirtual.edu.co</a></p>
+                        <p><b>Email: </b> <a href="https://www.google.com/intl/es-419/mail/help/about.html" target="_blank">jygarzon@uniquindio.edu.co</a></p>
+                        <p><b>Dirección</b></p>
+                        <p>Carrera 15 Calle 12 Norte</p>
+                        <p>Armenia, Quindío, Colombia</p>
+                        <p><b>Tel.: </b>+57 (6) 7359300 </p>
+                        <p><b>Quejas y Reclamos: </b>018000 96 35 78 opción 5 </p>
+                        <p><b>Denuncias actos de corrupción: </b>+57 (6) 7359416 Ext.416</p>
+                    </div>
+                          
+                    <div class="social-icons"> 
+                      <ul class="nomargin">
+                          <a href="https://www.facebook.com/bootsnipp" target="_blank"><i class="fa fa-facebook-square fa-3x social-fb" id="social"></i></a>
+                          <a href="https://twitter.com/bootsnipp" target="_blank"><i class="fa fa-twitter-square fa-3x social-tw" id="social"></i></a>
+                          <a href="https://plus.google.com/+Bootsnipp-page" target="_blank"><i class="fa fa-google-plus-square fa-3x social-gp" id="social"></i></a>
+                          <a href="mailto:bootsnipp@gmail.com" target="_blank"><i class="fa fa-envelope-square fa-3x social-em" id="social"></i></a>   
+                      </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+            <ul class="list-unstyled clear-margins"><!-- widgets -->      
+                <li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    <h1 class="title-widget">Facultad de Ingenieria</h1>
+                    <ul>
+                        <img src="{{asset('images/facultadIngenieria.png')}}" class="img-responsive" alt="Uniquindio">
+                    </ul>
+                </li>         
+            </ul>
+        </div><!-- widgets column left end -->
+
     </div>
-
-    <div class="footer-center">
-
-      <div>
-
-        <p><span>Carrera 15 Calle 12 Norte</span></p>
-        <ul class="divider"></ul>
-        <p><span >Armenia, Quindío, Colombia</span></p>
-        <ul class="divider"></ul>
-      </div>
-      <div>
-
-        <p><a href="#">jygarzon@uniquindio.edu.co</a></p>
-      </div>
-
-    </div>
-
-    <div class="footer-right">
-
-      <br>
-      <img src="{{asset('images/facul.png')}}" class="img-responsive" alt="Facultad">
-
-    </div>
-
+</div>
 </footer>
+<!--header-->
+
+<div class="footer-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <div class="copyright">
+          © Noviembre 2016, Facultad de Ingeniería - UNIQUINDIO, All rights reserved
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+        <div class="design">
+           <a href="#">Ingeniería </a> |  <a target="_blank" href="http://www.webenlance.com">Ingeniería de Sistemas y Computación & Franja Diurna</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
